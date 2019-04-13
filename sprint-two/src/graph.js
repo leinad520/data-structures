4bf7bde7c544b66b45cc44b5fdb3ed794c744f95
leinad520 <ksydaniel@gmail.com>
2019-04-12 19:21:40 -0700

    implement hasEdge function

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,30 +2,59 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  // edge
+  this.edges = {};
+  // node
+  this.nodes = []; 
+  
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
+  this.edges[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (let i = 0; i<this.nodes.length; i++) {
+    if (this.nodes[i] === node) return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i= 0; i < this.nodes.length; i++){
+    if (this.nodes[i] === node){
+      this.nodes.splice(i, 1);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return (this.edges[fromNode].includes(toNode));
+  
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode].push(toNode);
+  this.edges[toNode].push(fromNode);  
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  // loop through fromNodes array so to check if there are multiple edges
+  for (var i = 0; i < this.edges[fromNode].length; i++ ){
+    //if the toNode is present delete from array
+    if (this.edges[fromNode].includes(toNode)){
+      this.edges[fromNode].splice(i, 1); 
+    }
+
+  }  
+  
 };
 
 // Pass in a callback which will be executed on each node of the graph.
@@ -36,4 +65,14 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+// Implement a graph class, in pseudoclassical style, with the following properties:
+// It is an undirected graph. It does not have to be 'connected'.
+// An .addNode() method that takes a new node and adds it to the graph
+// A .contains() method that takes any node and returns a boolean reflecting whether it can be found in the graph
+// A .removeNode() method that takes any node and removes it from the graph, if present. All edges connected to that Node are removed as well.
+// An .addEdge() method that creates a edge (connection) between two nodes if they both are present within the graph
+// A .hasEdge() method that returns a boolean reflecting whether or not two nodes are connected
+// A .removeEdge() method that removes the connection between two nodes
+// A .forEachNode() method that traverses through the graph, calling a passed in function once on each node
+// What is the time complexity of the above functions?
 
